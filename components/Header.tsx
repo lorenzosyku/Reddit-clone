ca50f2bf110d0ed8f16d284e66001a585153e2f3
lorenzosyku <lorenzo.naomi1920@gmail.com>
2022-05-19 19:49:58 -0400

    added heroicons

---
+++ components/Header.tsx
@@ -0,0 +1,60 @@
+import Image from 'next/image'
+import React from 'react'
+import {
+  ChevronDownIcon,
+  HomeIcon,
+  SearchIcon,
+  MenuIcon,
+} from '@heroicons/react/solid'
+import {
+  BellIcon,
+  ChatIcon,
+  GlobeIcon,
+  PlusIcon,
+  SparklesIcon,
+  SpeakerphoneIcon,
+  VideoCameraIcon,
+} from '@heroicons/react/outline'
+
+function Header() {
+  return (
+    <div className="sticky top-0 z-50 flex bg-white px-4 py-2 shadow-sm">
+      <div className="relative h-10 w-20 flex-shrink-0 cursor-pointer">
+        <Image
+          objectFit="contain"
+          src="https://links.papareact.com/fqy"
+          layout="fill"
+        />
+      </div>
+
+      <div className="mx-7 flex items-center xl:min-w-[300px]">
+        <HomeIcon className="h-5 w-5" />
+        <p className="ml-2 hidden flex-1 lg:inline">Home</p>
+        <ChevronDownIcon className="h-5 w-5 " />
+      </div>
+
+      <form className="flex flex-1 items-center space-x-2 rounded-sm border border-gray-100 px-3 py-1">
+        <SearchIcon className="h-6 w-6 text-gray-400" />
+        <input
+          className="flex-1 bg-transparent outline-none"
+          type="text"
+          placeholder="Search Reddit"
+        />
+        <button type="submit" hidden />
+      </form>
+
+      <div className="flex">
+        <SparklesIcon className="icon"/>
+        <GlobeIcon className="icon"/>
+        <VideoCameraIcon className="icon"/>
+        <hr className="h-10 border border-gray-100"/>
+        <ChatIcon className="icon"/>
+        <BellIcon className="icon"/>
+        <PlusIcon className="icon"/>
+        <SpeakerphoneIcon className="icon"/>
+      </div>
+    </div>
+  )
+}
+
+export default Header
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,14 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="">
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Reddit Clone</title>
       </Head>
-      <h1>hello world</h1>
+      <Header />
     </div>
   )
 }

ba0c3bae94d1bd63573075405a28661656619bef
lorenzosyku <lorenzo.naomi1920@gmail.com>
2022-05-20 12:32:14 -0400

    added postBox and avatar

---
+++ components/PostBox.tsx
@@ -0,0 +1,24 @@
+import { useSession } from 'next-auth/react'
+import React from 'react'
+import Avatar from './Avatar'
+
+function PostBox() {
+  const { data: session } = useSession()
+  return (
+    <form className="sticky top-16 z-50 bg-white border rounded-md border-gray-300 p-2">
+      <div className="flex items-center space-x-3">
+        <Avatar seed="Loro" large/>
+        <input
+          disabled={!session}
+          className="bg-gray-50 p-2 pl-5 outline-none rounded-md flex-1"
+          type="text"
+          placeholder={
+            session ? 'Create a post by entering a title' : 'Sign in to post'
+          }
+        />
+      </div>
+    </form>
+  )
+}
+
+export default PostBox

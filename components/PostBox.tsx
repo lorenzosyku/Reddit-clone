896a1176975eb6ece5146d01e2892410ef293777
lorenzosyku <lorenzo.naomi1920@gmail.com>
2022-05-20 13:00:09 -0400

    buided the body section of the post box

--- components/PostBox.tsx
+++ components/PostBox.tsx
@@ -1,27 +1,76 @@
 import { useSession } from 'next-auth/react'
-import React from 'react'
+import React, { useState } from 'react'
 import Avatar from './Avatar'
-import { PhotographIcon,
-  LinkIcon,
-} from '@heroicons/react/outline'
+import { PhotographIcon, LinkIcon } from '@heroicons/react/outline'
+import { useForm } from 'react-hook-form'
+
+type FormData = {
+  postTitle: string
+  postBody: string
+  postImage: string
+  subreddit: string
+}
 
 function PostBox() {
   const { data: session } = useSession()
+  const [imageBoxOpen, setImageBoxOpen] = useState<boolean>()
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<FormData>()
   return (
-    <form className="sticky top-16 z-50 bg-white border rounded-md border-gray-300 p-2">
+    <form className="sticky top-16 z-50 rounded-md border border-gray-300 bg-white p-2">
       <div className="flex items-center space-x-3">
-        <Avatar seed="Loro" large/>
+        <Avatar seed="Loro" large />
         <input
+          {...register('postTitle', { required: true })}
           disabled={!session}
-          className="bg-gray-50 p-2 pl-5 outline-none rounded-md flex-1"
+          className="flex-1 rounded-md bg-gray-50 p-2 pl-5 outline-none"
           type="text"
           placeholder={
             session ? 'Create a post by entering a title' : 'Sign in to post'
           }
         />
-        <PhotographIcon className={`h-6 text-gray-300 cursor-pointer`}/>
-        <LinkIcon className="h-6 text-gray-300"/>
+        <PhotographIcon onClick={()=> setImageBoxOpen(!imageBoxOpen)} className={`h-6 cursor-pointer text-gray-300 ${imageBoxOpen && 'text-blue-300'}`} />
+        <LinkIcon className="h-6 text-gray-300" />
       </div>
+      {!!watch('postTitle') && (
+        <div className="flex flex-col py-2">
+          <div className="flex items-center px-2">
+            <p className="min-w-[90px]">Body:</p>
+            <input
+              className="m-2 flex-1 bg-blue-50 p-2 outline-none"
+              {...register('postBody')}
+              type="text"
+              placeholder="Text (optional)"
+            />
+          </div>
+
+          <div className="flex items-center px-2">
+            <p className="min-w-[90px]">Subreddit:</p>
+            <input
+              className="m-2 flex-1 bg-blue-50 p-2 outline-none"
+              {...register('subreddit')}
+              type="text"
+              placeholder="i.e. reactjs"
+            />
+          </div>
+
+          {imageBoxOpen && (
+            <div className="flex items-center px-2">
+            <p className="min-w-[90px]">Image URL:</p>
+            <input
+              className="m-2 flex-1 bg-blue-50 p-2 outline-none"
+              {...register('postImage')}
+              type="text"
+              placeholder="Optional"
+            />
+          </div>
+          )}
+        </div>
+      )}
     </form>
   )
 }

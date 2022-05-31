3456698b5110131f3c8e541feb0cf3d6db9f150e
lorenzosyku <lorenzo.naomi1920@gmail.com>
2022-05-31 19:16:48 -0400

    added feed and post components

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,14 +1,19 @@
 import { useQuery } from '@apollo/client'
 import React from 'react'
 import { GET_ALL_POSTS } from '../graphql/queries'
+import Post from './Post'
 
 
 function Feed() {
   const { data, error } = useQuery(GET_ALL_POSTS)
 
+  const posts: Post[] = data?.getPostList
+
   return (
-    <div>
-      
+    <div className="mt-5 space-y-4">
+      {posts?.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
     </div>
   )
 }

23ee19bcee0d03808f6e6059c80d361f1a065e74
lorenzosyku <lorenzo.naomi1920@gmail.com>
2022-06-01 09:46:38 -0400

    added top community section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,22 @@
+import { useQuery } from '@apollo/client'
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import { ChevronUpIcon } from '@heroicons/react/outline'
+import Avatar from '../components/Avatar'
 import Feed from '../components/Feed'
 import PostBox from '../components/PostBox'
+import { GET_SUBREDDITS_WITH_LIMIT } from '../graphql/queries'
+import SubredditRow from '../components/SubredditRow'
 
 const Home: NextPage = () => {
+  const { data } = useQuery(GET_SUBREDDITS_WITH_LIMIT, {
+    variables: {
+      limit: 10,
+    },
+  })
+
+  const subreddits: Subreddit[] = data?.getSubredditListLimit
+
   return (
     <div className="my-7 mx-auto max-w-5xl">
       <Head>
@@ -14,6 +27,19 @@ const Home: NextPage = () => {
 
       <div className="flex">
         <Feed />
+        <div className="sticky top-36 mx-5 mt-5 hidden h-fit min-w-[300px] rounded-md border border-gray-300 bg-white lg:inline">
+          <p className="text-md mb-1 p-4 pb-3 font-bold">Top Communities</p>
+
+          <div className="">
+            {subreddits?.map((subreddit, i) => (
+              <SubredditRow
+                key={subreddit.id}
+                topic={subreddit.topic}
+                index={i}
+              />
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   )
